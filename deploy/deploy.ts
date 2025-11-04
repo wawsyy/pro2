96e41a8e4b62be26cf2e9b5a6b0b16b5aa8dbaed
wawsyy <shiyu689@qq.com>
2025-11-04 13:00:00 -0800

    refactor: update deployment script for EncryptedSurvey contract

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -5,13 +5,13 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  const deployedFHECounter = await deploy("FHECounter", {
+  const deployedEncryptedSurvey = await deploy("EncryptedSurvey", {
     from: deployer,
     log: true,
   });
 
-  console.log(`FHECounter contract: `, deployedFHECounter.address);
+  console.log(`EncryptedSurvey contract deployed at: `, deployedEncryptedSurvey.address);
 };
 export default func;
-func.id = "deploy_fheCounter"; // id required to prevent reexecution
-func.tags = ["FHECounter"];
+func.id = "deploy_encrypted_survey"; // id required to prevent reexecution
+func.tags = ["EncryptedSurvey"];
